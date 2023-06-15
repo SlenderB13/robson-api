7f8b1d356861e9802d4716f3d6aa4217b1a95757
Lucas Silva <lguilherme8@gmail.com>
2023-06-14 22:49:58 -0300

    add: cors em controller de produto

--- src/main/java/com/robson/robson/Product/ProductController.java
+++ src/main/java/com/robson/robson/Product/ProductController.java
@@ -3,6 +3,7 @@ package com.robson.robson.Product;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = {"http://localhost:8080", "http://localhost:5173"})
 @RequestMapping("api/v1")
 public class ProductController {
     @Autowired
