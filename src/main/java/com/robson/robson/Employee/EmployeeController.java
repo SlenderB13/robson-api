8f33dccd731d22a6a4411bb9c7cf338b258e2a73
Lucas Silva <lguilherme8@gmail.com>
2023-06-14 22:51:18 -0300

    add: cors em controller de funcionario e annotations de controller

--- src/main/java/com/robson/robson/Employee/EmployeeController.java
+++ src/main/java/com/robson/robson/Employee/EmployeeController.java
@@ -3,11 +3,17 @@ package com.robson.robson.Employee;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@CrossOrigin(origins = {"http://localhost:8080", "http://localhost:5173"})
+@RequestMapping("api/v1")
 public class EmployeeController {
     @Autowired
     private EmployeeRepository employeeRepository;
